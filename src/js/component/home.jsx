51868098dd29fbce220ed87b90d244e48a890109
maoc86 <soymanuweb@gmail.com>
2023-08-25 02:33:24 +0000

    FINAL

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import NavBar from "./nav.jsx";
-import Jumbo from "./jumbotron.jsx"
+import Jumbo from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -14,9 +16,27 @@ const Home = () => {
 		<div>
 					<Jumbo />
 				</div>
+				<div className="row row-cols-1 row-cols-md-4 g-4">
+					<div className="col">
+						<Card image="https://images.unsplash.com/photo-1509470475192-4516c145f8a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" title="Street Photography" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." label="Find out more!" />
+					</div>
+					<div className="col">
+						<Card image="https://images.unsplash.com/photo-1509470475192-4516c145f8a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" title="Street Photography" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." label="Find out more!" />
+					</div>
+					<div className="col">
+						<Card image="https://images.unsplash.com/photo-1509470475192-4516c145f8a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" title="Street Photography" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." label="Find out more!" />
+					</div>
+					<div className="col">
+						<Card image="https://images.unsplash.com/photo-1509470475192-4516c145f8a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" title="Street Photography" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." label="Find out more!" />
+					</div>
+					</div>
+					<div>
+				<Footer />
+			</div>
 		
 
 		</div>
+		
 	);
 };
 
--- src/js/component/nav.jsx
+++ src/js/component/nav.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const NavBar = () => {
 	return (
-<nav className="navbar navbar-expand-md navbar-dark bg-dark">
+<nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
   <a className="navbar-brand" href="#">Start Bootstrap</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
+  <ul className="navbar-nav mr-auto mt-2 mt-lg-0 ms-auto me-5">
       <a className="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
       <a className="nav-item nav-link" href="#">About</a>
       <a className="nav-item nav-link" href="#">Service</a>
       <a className="nav-item nav-link" href="#">Conctact</a>
-    </div>
+    </ul>
   </div>
 </nav>
 );
